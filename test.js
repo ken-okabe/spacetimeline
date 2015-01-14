5d45a9b7c4224944a48027ee628e72d3a5e83642
kenokabe <kenokabe@gmail.com>
2015-01-14 22:29:04 +0900

    commit

--- test.js
+++ test.js
@@ -3,20 +3,66 @@
 /* jshint sub: true */
 /* global window,document, $,alert,history */
 
+"use strict";
+// -- stdlib --
+var pure = function(a)
+{
+  return function(_)
+  {
+    return a;
+  };
+};
+// pure :: a -> IO a
+var bind = function(m)
+{
+  return function(f)
+  {
+    return function(_)
+    {
+      return f(m())();
+    };
+  };
+};
+// bind :: IO a -> (a -> IO b) -> IO b
+var exec = function(m)
+{
+  return m();
+};
+// exec :: IO a -> a
+var wrap = function(f)
+{
+  return function(a)
+  {
+    return function(_)
+    {
+      return f(a);
+    };
+  };
+};
+// -- runtime --
+Object.defineProperty(global, "world",
+{
+  set: exec
+});
+
+// wrap :: (a -> b) -> (a -> IO b)
+var log = wrap(console.log.bind(console));
+
+
 var ___ = require('./spacetimeline');
 
 
 //var timelineCapacity = moment.duration(40, 'seconds');
-console.log('----');
+world = log('----');
 //var ___a = ___(timelineCapacity);
 var ___a = ___();
 
-___a.compute(function()
+world = wrap(___a.compute(function()
 {
-  console.log('a: ' + ___a.value(___('NOW')));
-});
+  world = log('a: ' + ___a.value(___('NOW')));
+}));
 
 var interval = setInterval(function()
 {
-  ___a.appear(___('NOW').toString());
+  world = wrap(___a.appear(___('NOW').toString()));
 }, 1000);
